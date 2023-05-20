ff3ecef5189937133a06fb0f1d347e82b8687c24
rajibrahman74e <rajiburrahman74@gmail.com>
2023-05-20 14:40:33 +0600

    [ADD]-all-Toys-components-apply-syles-and-dynamic-ratings

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -7,24 +7,21 @@ const AllToys = () => {
   const [limit, setLimit] = useState(20);
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredToys, setFilteredToys] = useState([]);
-  
 
   // update filter toys
   useEffect(() => {
     const updatedFilteredToys = allToys.filter((toy) => {
-      const name = toy.name || ""; 
+      const name = toy.name || "";
 
       return name.toLowerCase().includes(searchTerm.toLowerCase());
     });
     setFilteredToys(updatedFilteredToys);
   }, [allToys, searchTerm]);
 
-
   // search impliment here
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
-
 
   // limite data chanage
   const handleLimitChange = (event) => {
@@ -34,44 +31,52 @@ const AllToys = () => {
   const displayedToys = filteredToys.slice(0, limit);
 
   return (
-    <div className="max-w-7xl mx-auto px-4">
-      <h1 className="text-4xl">Your bookings {displayedToys.length}</h1>
+    <div className="max-w-7xl mx-auto px-6">
+      <h1 className="text-3xl text-center my-12 font-bold">
+        All <span className="text-blue-800 font-extrabold">Toys</span>
+      </h1>
       <div className="overflow-x-auto w-full">
-        <div className="mt-4">
-          <input
-            type="text"
-            placeholder="Search by Toy Name"
-            className="border rounded px-4 py-2"
-            value={searchTerm}
-            onChange={handleSearch}
-          />
-        </div>
-        <div className="mt-4">
-          <label htmlFor="limitSelect" className="mr-2">
-            Show:
-          </label>
-          <select
-            id="limitSelect"
-            className="border rounded px-4 py-2"
-            value={limit}
-            onChange={handleLimitChange}
-          >
-            <option value={20}>20</option>
-            <option value={30}>30</option>
-            <option value={50}>50</option>
-          </select>
+        <div className="pb-12 pt-4 flex items-center justify-center">
+          <div className="flex items-center w-2/3 text-start">
+            <label className="font-semibold text-xl w-1/3">
+              Search your favorite
+              <span className="font-extrabold text-blue-800"> toys:</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Search by Toy Name"
+              className="border rounded px-4 py-2 w-2/3 -ml-6"
+              value={searchTerm}
+              onChange={handleSearch}
+            />
+          </div>
+          <div className="w-1/3 text-end">
+            <label htmlFor="limitSelect" className="mr-2 font-extrabold ">
+              Show:
+            </label>
+            <select
+              id="limitSelect"
+              className="border rounded px-4 py-2"
+              value={limit}
+              onChange={handleLimitChange}
+            >
+              <option value={20}>20</option>
+              <option value={30}>30</option>
+              <option value={50}>50</option>
+            </select>
+          </div>
         </div>
 
         <table className="table w-full">
-          <thead className="text-center">
+          <thead className="text-cente">
             <tr>
-              <th>Image</th>
-              <th>Seller</th>
-              <th>Name</th>
-              <th>Quantity</th>
-              <th>Price</th>
-              <th>Ratings</th>
-              <th></th>
+              <th className="text-blue-800 font-bold text-[15px] text-center">Image</th>
+              <th className="text-blue-800 font-bold text-[15px]">Seller name</th>
+              <th className="text-blue-800 font-bold text-[15px]">toy Name</th>
+              <th className="text-blue-800 font-bold text-[15px]">Quantity</th>
+              <th className="text-blue-800 font-bold text-[15px]">Price</th>
+              <th className="text-blue-800 font-bold text-[15px] text-center">Ratings</th>
+              <th className="text-blue-800 font-bold text-[15px]"></th>
             </tr>
           </thead>
           <tbody>
--- src/Pages/AllToys/Toys.jsx
+++ src/Pages/AllToys/Toys.jsx
@@ -1,23 +1,42 @@
 import { Link } from "react-router-dom";
+import { StarIcon } from "@heroicons/react/24/solid";
+import clsx from "clsx";
 
 const Toys = ({ toy }) => {
-  const { _id, seller, price, rating, name, quantity, picture } = toy;
-  console.log(toy);
+  const { _id, seller, price, name, quantity, picture } = toy;
+  // console.log(toy);
   return (
-    <tr className="text-center mx-auto">
+    <tr className="t mx-auto">
       <td>
         <div className="avatar">
           <div className="rounded w-24 h-24">{<img src={picture} />}</div>
         </div>
       </td>
-      <td>{seller}</td>
-      <td>{name}</td>
-      <td>{quantity}</td>
-      <td> {price}</td>
-      <td>{rating}</td>
+      <td className="text-lg font-bold">{seller}</td>
+      <td className="font-semibold">{name}</td>
+      <td className="font-bold text-center">{quantity}</td>
+      <td className="font-bold"> {price}</td>
+      <td>
+        <div className="flex items-center justify-center">
+          {Array.from({ length: 5 }).map((_, index) => {
+            const rating = Math.round(toy.rating);
+            return (
+              <StarIcon
+                key={index}
+                className={clsx(
+                  "h-5 w-5",
+                  index < rating ? "text-yellow-400" : "text-gray-300"
+                )}
+              />
+            );
+          })}
+        </div>
+      </td>
       <th className="text-center">
         <Link to={`/alltoys/${_id}`}>
-          <button className="border-0 btn btn-outline">View Details</button>
+          <button className="border-0 py-2 text-white hover:bg-black px-4 bg-warning">
+            View Details
+          </button>
         </Link>
       </th>
     </tr>
